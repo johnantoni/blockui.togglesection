a91001759995d03f4fd35e4dc896f75df39c1951
John Antoni Griffiths <john@johnantoni.com>
2011-07-07 14:28:26 -0400

    add optional delay on slidedown

--- jquery.blockui.togglesection.js
+++ jquery.blockui.togglesection.js
@@ -6,19 +6,25 @@
 //   http://jquery.malsup.com/block
 //
 // params : parameters to give to blockUI
+// callback : plugin callback
+//
+// author: John Griffiths
+// version: 0.3
 
 (function( $ ){
 
-  $.fn.toggleSection = function(params) {
+  $.fn.toggleSection = function(params, callback) {
     var el = this; // get element
 
     // set default params (no message), merging with ones given
-    var params = $.extend({ message: null }, params);  
+    // ...also augment toggle to include delay on slideDown
+    var params = $.extend({ message: null, delaySlide: 0 }, params);
 
     if (el.is(":hidden")) {
       // slide down instructions then block preceeding sections
-      el.slideDown('slow', 'linear')
-        .nextAll().block(params);
+      el.nextAll().block(params)
+        .end()
+        .delay(params.delaySlide).slideDown('slow', 'linear');
     }
     
     else {
@@ -27,6 +33,11 @@
         .end()
         .slideUp('slow', 'linear');
 
+    }
+    
+    // allow the plugin to support callbacks
+    if (typeof callback == 'function') {
+        callback.call(this);
     }
     
   };
