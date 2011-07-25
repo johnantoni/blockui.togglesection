e05352cbc253e35a122a9d4fb51ae54c60bf83f0
John Antoni Griffiths <john@johnantoni.com>
2011-07-25 11:33:36 -0700

    affectOnly param

--- jquery.blockui.togglesection.js
+++ jquery.blockui.togglesection.js
@@ -8,8 +8,11 @@
 // params : parameters to give to blockUI
 // callback : plugin callback
 //
+// delaySlide : milliseconds to delay slideopen effect
+// affectOnly : affect only certain elements (e.g. section elements and not footer), e.g. "section, .advert"
+//
 // author: John Griffiths
-// version: 0.3
+// version: 0.3.1
 
 (function( $ ){
 
@@ -18,18 +21,18 @@
 
     // set default params (no message), merging with ones given
     // ...also augment toggle to include delay on slideDown
-    var params = $.extend({ message: null, delaySlide: 0 }, params);
+    var params = $.extend({ message: null, delaySlide: 0, affectOnly: "" }, params);
 
     if (el.is(":hidden")) {
       // slide down instructions then block preceeding sections
-      el.nextAll().block(params)
+      el.nextAll(params.affectOnly).block(params)
         .end()
         .delay(params.delaySlide).slideDown('slow', 'linear');
     }
     
     else {
       // unblock proceeding sections then slide up instructions
-      el.nextAll().unblock()
+      el.nextAll(params.affectOnly).unblock()
         .end()
         .slideUp('slow', 'linear');
 
